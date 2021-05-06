b6f5f69b0b8e2ad5e093c798fe1a0d87e9373258
Kou <Kou@otinoMacBook-Pro.local>
2021-05-06 21:13:09 +0900

    add q7~8q

--- task.rb
+++ task.rb
@@ -5,7 +5,7 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-  names.push("斎藤")
+  names.<<("斎藤")
   p names
 end
 
@@ -14,9 +14,14 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-  array1.push(array2)
-  array1.flatten!
-  p array1
+  array = array1 + array2
+  p array
+
+  #別解
+  #array1.push(array2)
+  #array1.flatten!
+  #p array1
+
 end
 
 def q3
@@ -40,14 +45,7 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-  puts "array1 = []"
-  puts "# 実行結果"
   p array1.empty?
-
-  puts "" #改行
-
-  puts "array2 = [1, 5, 8, 10]"
-  puts "# 実行結果"
   p array2.empty?
 end
 
@@ -63,7 +61,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -72,7 +70,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
